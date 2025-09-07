3801fe97b20b795d0c649e788e204b1d7cd17f97
mikanbox <shuaki@amazon.co.jp>
2025-09-07 15:35:00 +0900

    ci: 🎡 add novaact test commands

--- tests/main.py
+++ tests/main.py
@@ -1,1 +1,30 @@
-print("hell world")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from nova_act import NovaAct
+
+def run_test():
+
+    nova = NovaAct(starting_page="http://localhost:5173/", 
+               ignore_https_errors=True, 
+               headless=True,
+               )
+    
+    nova.start()
+    nova.act("FlexMatch JSON ルールをロードするボタンをクリックして")
+    nova.act("いずれかのルールを選択して")
+    nova.act("OKを押して JSON をロードして")
+    nova.act("Visualize ボタンを押して")
+
+if __name__ == "__main__":
+    print("Start Testing ")
+
+    sys.exit(run_test())
+
+
+
+
+
+
+
